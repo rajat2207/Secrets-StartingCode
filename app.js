466651bcb98be48a8952984769d7650089f7b98c
Rajat <rajatraj2207@gmail.com>
2022-03-14 16:17:30 +0530

    level 1 Security added

--- app.js
+++ app.js
@@ -1,26 +1,70 @@
 //jshint esversion:6
-const express=require('express');
-const bodyParser=require('body-parser');
-const ejs=require('ejs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
 
-const app=express();
+const app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',function(req,res){
+mongoose.connect('mongodb://localhost:27017/usersDB', { useNewUrlParser: true })
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.get('/login',function(req,res){
+app.get('/login', function (req, res) {
     res.render('login');
 });
 
-app.get('/register',function(req,res){
+app.get('/register', function (req, res) {
     res.render('register');
 })
 
-app.listen(3000,function(){
+
+app.post('/register', function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("New User Added");
+            res.render('secrets')
+        }
+    }
+    )
+})
+
+app.post('/login', function (req, res) {
+    const usrName=req.body.username;
+    const pwd=req.body.password;
+
+    User.find({email:usrName,password:pwd},function(err,result){
+        if(err){
+            console.log(err);
+        }else if(result){
+            res.render('secrets');
+            console.log('user found');
+        }else{
+            res.render('login');
+        }
+    })
+})
+
+app.listen(3000, function () {
     console.log("Server running on Port 3000");
 })
